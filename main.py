d51331d1a80fe0ebb6978359230ea947f825c500
Bhavesh <bhavesh189kumar@gmail.com>
2021-09-01 11:22:52 +0530

    Simple scheduler to run everyday at 8AM morning.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import atexit
 from flask import Flask
 from services.cphelper_service import CpHelper
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 app = Flask(__name__)
+
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+scheduler.add_job(
+    func=lambda: print("hey"), trigger="cron", hour="8", minute="0", day="*"
+)
+
+atexit.register(lambda: scheduler.shutdown())
 
 
 @app.route("/")
