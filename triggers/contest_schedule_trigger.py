0f5d97ded4ddefffbe78270373ce500b3b3d308a
Bhavesh <56588114+bhavesh-20@users.noreply.github.com>
2021-09-01 17:54:17 +0530

    Rework on discord message. (#1)
    * Combined and rephrased the message to be sent to discord server for contest reminders.
    
    * error fix declaring reminder_message and webhook URL changes for test bot.

--- triggers/contest_schedule_trigger.py
+++ triggers/contest_schedule_trigger.py
@@ -6,7 +6,7 @@ import requests
 
 class ContestTrigger:
 
-    ERR_WEBHOOK_URL = "https://discord.com/api/webhooks/882509204328415263/RByHwVsdpCkidfVoX5ubkP32_9b3TaX7jIrmhJFGOVd0PABRoxJBQHx8B038sg1TgKws"
+    ERR_WEBHOOK_URL = "https://discord.com/api/webhooks/882575945859498014/8h0dSCyKoQwHZuadGM4S7gW0E7YzKBusisWsxi4TOx3s6FXeAzZxO5DwBfncGOAWMydU"
     CONTEST_WEBHOOK_URL = "https://discord.com/api/webhooks/882514149731622932/nPY-RUPjxzLqP9v3k8HrMBWK1j2-NTSHcRW4PcLHDiugbiZcP6Vm81EZ8ojbtExbIkqJ"
 
     @classmethod
@@ -20,12 +20,11 @@ class ContestTrigger:
             }
             requests.post(cls.ERR_WEBHOOK_URL, data=data)
         else:
-            messages = resp.get("message")
-            for message in messages:
-                data = {
-                    "content": message,
-                }
-                requests.post(cls.CONTEST_WEBHOOK_URL, data=data)
+            message = resp.get("message")
+            data = {
+                "content": message,
+            }
+            requests.post(cls.CONTEST_WEBHOOK_URL, data=data)
         return None
 
     @classmethod
@@ -41,16 +40,15 @@ class ContestTrigger:
         if len(contest_details) == 0:
             return False, {"message": "No Contest data available for today."}
 
-        messages = []
+        reminder_message = "Hey @everyone, Daily Contest Reminder" + "\n"
 
         for contest in contest_details:
-            message = "Hey @everyone" + "\n"
-            message += (
-                f"There is a contest - {contest['event']}, {contest['starts_at']['type']} at {contest['starts_at']['value']}, hosted by {contest['host']}"
+            message = (
+                f"{contest['event']} starts {contest['starts_at']['type']} at {contest['starts_at']['value']}, hosted by {contest['host']}"
                 + "\n"
             )
             message += f"Duration - {contest['duration']}" + "\n"
-            message += f"Link - {contest['link']}"
-            messages.append(message)
+            message += f"Link - {contest['link']}" + "\n"
+            reminder_message += message
 
-        return True, {"message": messages}
+        return True, {"message": reminder_message}
