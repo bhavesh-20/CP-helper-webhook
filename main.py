bbc383a5ab15281af714e7c98d29c132bc7f8291
Bhavesh <bhavesh189kumar@gmail.com>
2021-09-02 11:54:02 +0530

    changed web command from gunicorn to python main.py and included debug and use_reloader parameters in app.run which are set to false.

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler(timezone="Asia/Kolkata")
 scheduler.start()
 
 scheduler.add_job(
-    func=ContestTrigger.trigger, trigger="cron", hour="5", minute="30", day="*"
+    func=ContestTrigger.trigger, trigger="cron", hour="9", minute="0", day="*"
 )
 
 atexit.register(lambda: scheduler.shutdown())
@@ -28,4 +28,4 @@ def contests():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False, use_reloader=False)
